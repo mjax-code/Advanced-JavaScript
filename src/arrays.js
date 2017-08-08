2c8a95dbb2147a1d7c6cd14f9332c7116de51b75
Mjax <matt.d.jax@gmail.com>
2017-08-08 13:49:59 -0700

    unfinished arrays project

--- src/arrays.js
+++ src/arrays.js
@@ -8,28 +8,56 @@ const each = (elements, cb) => {
   // Iterates over a list of elements, yielding each in turn to the `cb` function.
   // This only needs to work with arrays.
   // based off http://underscorejs.org/#each
+  const modifiedArr = [];
+  for (let i = 0; i < elements.length; i++) {
+    modifiedArr.push(cb(elements[i], i));
+  }
+  return modifiedArr;
 };
 
 const map = (elements, cb) => {
   // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
   // Return the new array.
+  const modifiedArr = [];
+  for (let i = 0; i < elements.length; i++) {
+    modifiedArr.push(cb(elements[i]));
+  }
+  return modifiedArr;
 };
 
 const reduce = (elements, cb, memo = elements.shift()) => {
   // Combine all elements into a single value going from left to right.
   // Elements will be passed one by one into `cb`.
   // `memo` is the starting value.  If `memo` is undefined then make `elements[0]` the initial value.
+  let sum = memo;
+  for (let i = 0; i < elements.length; i++) {
+    sum += elements[i];
+  }
+  return sum;
 };
 
 const find = (elements, cb) => {
   // Look through each value in `elements` and pass each element to `cb`.
   // If `cb` returns `true` then return that element.
   // Return `undefined` if no elements pass the truth test.
+  for (let i = 0; i < elements.length; i++) {
+    if (cb(elements[i])) {
+      return elements[i];
+    }
+  }
+  return undefined;
 };
 
 const filter = (elements, cb) => {
   // Similar to `find` but you will return an array of all elements that passed the truth test
   // Return an empty array if no elements pass the truth test
+  const newArr = [];
+  for (let i = 0; i < elements.length; i++) {
+    if (cb(elements[i])) {
+      newArr.push(elements[i]);
+    }
+  }
+  return newArr;
 };
 
 const flatten = (elements) => {
